96124e1c93e078fddd92269af350f4d07b4a3531
eduardo ellwanger <dudi.cap07@gmail.com>
2025-10-20 08:54:02 -0300

    Start with React, feat: created Nav and Header, bugs: header is not displaying on screen, fix: button placement

---
+++ portifolio-v2/portifolioReact/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header/Header.jsx";
+
+function App() {
+  return (
+    <>
+      <div classeName="Header">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ portifolio-v2/portifolioReact/src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "../css/mainPage.css";
+import Nav from "./Nav";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div class="header">
+      <h2>Ellwanger Edward</h2>
+      <Nav />
+    </div>
+  );
+};
+
+export default Header;
